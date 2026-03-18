27906de0f425cb8148e3b69e9c63c6271a2be3d3
bentagami <bentagami@gmail.com>
2026-03-18 00:21:24 -0700

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://yourname.com', // ← replace with your domain
+  site: 'bentagami.com', // ← replace with your domain
 });
